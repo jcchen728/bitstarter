1613d670d56c4ad279803bde79b9f03234c6cbc1
jcchen728 <jc.chen728@gmail.com>
2013-07-11 07:36:08 +0000

    reading from index

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var fs = require('fs');
+var inFile = "index.html";
+var buf = fs.readFileSync(inFile);
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buf.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
